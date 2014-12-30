c7b8f9e89d70f53e90de7e257d126af45c98f4e3
eduardsakaev <EduardSakaev@gmail.com>
2014-12-30 18:20:52 +0200

    Implemented checkstyle in Percolation

--- Percolation/Percolation_java_project/src/Percolation.java
+++ Percolation/Percolation_java_project/src/Percolation.java
@@ -14,100 +14,125 @@
 
 public class Percolation 
 {
-   private int grid[];
+   private int [] grid;
    private int igridSize;
    private WeightedQuickUnionUF weightedquf;
-	
-   public Percolation(int N) // create N-by-N grid, with all sites blocked
+
+   /*---------------------------------------------------------
+    *  // create N-by-N grid, with all sites blocked
+    *  
+    *---------------------------------------------------------*/
+   public Percolation(int N) 
    {
-	   grid = new int[N * N + 1];
-	   for (int i = 0; i < N * N; ++i)
-		   grid[i] = 0;                                    //check if is open
-	   
-	   igridSize = N;                                     //Numbers of raw/columns
-	   
-	   weightedquf = new WeightedQuickUnionUF(N * N + 2); // +2 means 0 - top  N + 1 - bottom
+       grid = new int[N * N + 1];
+       for (int i = 0; i < N * N; ++i)
+            grid[i] = 0;          //check if is open
+         
+       igridSize = N;        //Numbers of raw/columns   
+       // +2 means 0 - top  N + 1 - bottom
+       weightedquf = new WeightedQuickUnionUF(N * N + 2);
    }      
-   
-   public void open(int i, int j) // open site (row i, column j) if it is not open already
-   { 
-	   int id    = GetGridIndex(i, j);
-	   
-	   if (j + 1 <= igridSize) //right 
-	   {
-		   int iright = GetGridIndex(i, j + 1);
-		   if (!isOpen(i, j + 1) && !weightedquf.connected(id, iright))
-			   weightedquf.union(id, iright);
-	   }
-	   
-	   if (j - 1 > 0) //left
-	   {
-		   int ileft = GetGridIndex(i, j - 1);
-		   if (!isOpen(i, j - 1) && !weightedquf.connected(id, ileft))
-			   weightedquf.union(id, ileft);
-	   }
-	   
-	   if (i - 1 > 0) //top
-	   {
-		   int itop = GetGridIndex(i - 1, j);
-		   if (!isOpen(i - 1, j) && !weightedquf.connected(id, itop))
-			   weightedquf.union(id, itop);
-	   }
-	   
-	   if (i + 1 <= igridSize) //bottom
-	   {
-		   int ibottom = GetGridIndex(i + 1, j);
-		   if (!isOpen(i + 1, j) && !weightedquf.connected(id, ibottom))
-			   weightedquf.union(id, ibottom);
-	   }
-	   
-	   grid[id] = 1;
-	   
-	   if (i == 1)
-		   weightedquf.union(0, id);
-	   
-	   for (i = 1; i <= igridSize; ++i)
-	   {
-		   id = GetGridIndex(igridSize, i);
-		   if (weightedquf.connected(0, id))
-		   {
-			   weightedquf.union(id, igridSize * igridSize + 1);
-			   break;
-		   }
-		   
-	   }
-		   
+
+   /*---------------------------------------------------------
+    *  open site (row i, column j) if it is not open already
+    *  
+    *---------------------------------------------------------*/
+   public void open(int i, int j) 
+   {  
+       int id    = getGridIndex(i, j);
+       
+       if (j + 1 <= igridSize) //right 
+       {
+           int iright = getGridIndex(i, j + 1);
+           if (!isOpen(i, j + 1) && !weightedquf.connected(id, iright))
+           weightedquf.union(id, iright);
+       }
+       if (j - 1 > 0) //left
+       {
+           int ileft = getGridIndex(i, j - 1);
+           if (!isOpen(i, j - 1) && !weightedquf.connected(id, ileft))
+           weightedquf.union(id, ileft);
+       }
+
+       if (i - 1 > 0) //top
+       {
+           int itop = getGridIndex(i - 1, j);
+           if (!isOpen(i - 1, j) && !weightedquf.connected(id, itop))
+           weightedquf.union(id, itop);
+       }
+
+       if (i + 1 <= igridSize) //bottom
+       {
+           int ibottom = getGridIndex(i + 1, j);
+           if (!isOpen(i + 1, j) && !weightedquf.connected(id, ibottom))
+           weightedquf.union(id, ibottom);
+       }
+
+       grid[id] = 1;
+
+       if (i == 1)
+       weightedquf.union(0, id);
+
+       for (int k = 1; k <= igridSize; ++k)
+       {
+           id = getGridIndex(igridSize, k);
+           if (weightedquf.connected(0, id))
+           {
+               weightedquf.union(id, igridSize * igridSize + 1);
+               break;
+           }
+       }
+
    }          
-   public boolean isOpen(int i, int j) // is site (row i, column j) open?
+   
+   /*---------------------------------------------------------
+    *  is site (row i, column j) open?
+    *  
+    *---------------------------------------------------------*/
+   public boolean isOpen(int i, int j) 
    {
-	   int id = GetGridIndex(i, j);
-	   boolean isOpen;
-	   isOpen = (grid[id] != 0) ? false : true;
-	   return isOpen;
+       int id = getGridIndex(i, j);
+       boolean bOpen = true;
+       if (grid[id] != 0)
+       bOpen =  false;
+       return bOpen;
    }     
    
-   public boolean isFull(int i, int j) // is site (row i, column j) full?
+   /*---------------------------------------------------------
+    *  // is site (row i, column j) full?
+    *  
+    *---------------------------------------------------------*/
+   public boolean isFull(int i, int j) 
    {
-	   int id = GetGridIndex(i, j);
-	   boolean is_connected = false;
-	   if (weightedquf.connected(0, id))
-		   is_connected =  true;
-	   
-	   return is_connected;
+       int id = getGridIndex(i, j);
+       boolean bConnected = false;
+       if (weightedquf.connected(0, id))
+       bConnected =  true;
+       return bConnected;
    } 
    
-   public boolean percolates() // does the system percolate?
+   
+   /*---------------------------------------------------------
+    *  // does the system percolate?
+    *  
+    *---------------------------------------------------------*/
+   public boolean percolates() 
    {
-	   int ibottom = igridSize * igridSize + 1;
-	   boolean is_percolate = false;
-	   if (weightedquf.connected(0, ibottom) || weightedquf.connected(ibottom, 0))
-			   is_percolate = true;
-	   return is_percolate;
+       int ibottom = igridSize * igridSize + 1;
+       boolean bPercolate = false;
+       if (weightedquf.connected(0, ibottom) 
+           || weightedquf.connected(ibottom, 0))
+           bPercolate = true;
+       return bPercolate;
    }             
 
-   private int GetGridIndex(int i, int j)
+   /*---------------------------------------------------------
+    *  translate i, j raw into sequence.
+    *  
+    *---------------------------------------------------------*/
+   private int getGridIndex(int i, int j)
    {
-	   return (i - 1) * igridSize + j;
+       return (i - 1) * igridSize + j;
    }
    //public static void main(String[] args);   // test client (optional)
 }
--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Algorithm_practice_1
 
 Analysis of algorith, Princeton univercity
 
+need to copy algs4 to ~/algs4
+
+checkstyle-algs4 Path_to_java.java
+findbug the same
+
