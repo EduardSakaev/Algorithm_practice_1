6c15e9854a7cc287cd45c8160cf417272b572d31
eduardsakaev <EduardSakaev@gmail.com>
2015-02-12 19:57:50 +0200

    Added test variant of Collinear project

---
+++ Collinear/Collinear/src/Brute.java
@@ -0,0 +1,23 @@
+/*************************************************************************
+ * Name:
+ * Email:
+ *
+ * Compilation:  javac Point.java
+ * Execution:
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ *
+ *************************************************************************/
+
+public class Brute {
+   public static void main(String[] args) {
+	   Point p1 = new Point (1,1);
+	   Point p2 = new Point (1,1);
+	   Point p3 = new Point (1,1);
+	   p1.SLOPE_ORDER.compare(p2, p3);
+       
+       //1. Save points in the array
+       
+   }
+}
---
+++ Collinear/Collinear/src/Point.java
@@ -0,0 +1,81 @@
+/*************************************************************************
+ * Name:
+ * Email:
+ *
+ * Compilation:  javac Point.java
+ * Execution:
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ *
+ *************************************************************************/
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+
+	private final int x;                              // x coordinate
+	private final int y;                              // y coordinate
+	
+	//compare points by slope
+	public final Comparator<Point> SLOPE_ORDER = new Comparator<Point>() // YOUR DEFINITION HERE
+	{
+		 @Override
+		 public int compare(Point p0, Point p1)
+		 {
+		     double s0 = slopeTo(p0);
+		     double s1 = slopeTo(p1);
+		     return Double.compare(s0, s1);
+		 }
+	};
+	
+	// create the point (x, y)
+	public Point(int x, int y) {
+	    /* DO NOT MODIFY */
+	    this.x = x;
+	    this.y = y;
+	}
+	
+	// plot this point to standard drawing
+	public void draw() {
+	    /* DO NOT MODIFY */
+	    StdDraw.point(x, y);
+	}
+	
+	// draw the line segment from this point to that point
+	public void drawTo(Point that) {
+	    /* DO NOT MODIFY */
+	    StdDraw.line(this.x, this.y, that.x, that.y);
+	}
+	
+	// slope between this point and that point
+	public double slopeTo(Point that) {
+	    /* YOUR CODE HERE */
+	    double slope = (that.y * 1.0 - this.y)/(that.x * 1.0 - this.x);
+	    return slope;
+	}
+	
+	// is this point lexicographically smaller than that one?
+	// comparing y-coordinates and breaking ties by x-coordinates
+	public int compareTo(Point that) {
+	    /* YOUR CODE HERE */
+	    if(this.y < that.y) return 1;
+	    else if(this.y == that.y) {
+	        if(this.x < that.x) return 1;
+	    else return 0;
+	    }
+	    else return 0;
+	} 
+	
+	// return string representation of this point
+	public String toString() {
+	    /* DO NOT MODIFY */
+	    return "(" + x + ", " + y + ")";
+	}
+	
+	// unit test
+	public static void main(String[] args) {
+	    /* YOUR CODE HERE */
+	}
+}
